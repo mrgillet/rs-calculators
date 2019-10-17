0b7fa2753e4ad94913c75fe8450083426073a856
Andrejs Gubars <andrejs.gubars@Andrejss-MacBook-Pro.local>
2019-10-17 20:07:30 +0100

    fresh

--- assets/src/scripts/app.js
+++
@@ -1,4 +0,0 @@
-/**
- * The main entry point into the site. This file gets transpiled
- * and bundled using Webpack.
- */
--- tools/gulp/build.js
+++
@@ -1,3 +0,0 @@
-module.exports = (gulp, shared) => {
-  gulp.task("build", gulp.series("sass", "jekyll:build"));
-};
